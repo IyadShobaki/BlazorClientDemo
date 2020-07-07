ff696c6c6da5bd417184808b244aa5d7a0bdefa2
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-07 13:19:50 -0400

    Updating Data
    Allow the user to update the order name inside the Order/Display page.

--- BlazorClientDemoApp/Models/OrderUpdateModel.cs
+++ BlazorClientDemoApp/Models/OrderUpdateModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,10 @@ namespace BlazorClientDemoApp.Models
     public class OrderUpdateModel
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an order name")]
+        [DisplayName("Name for the order")]
         public string OrderName { get; set; }
 
     }
